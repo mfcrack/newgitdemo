df057c3eac76fb1fc1ce309e86ee9b74bd0a19cb
xli65 <xiang.li@dxc.com>
2021-11-03 19:02:52 +0800

    111

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
   public static void main(String args[]){
-     sysout('初始状态2021 11 03 18:46 created')
+     sysout('初始状态2021 11 03 18:46 created   789')
 	 sout('zhenyu') added at 11 03 18:55
   }
 }
